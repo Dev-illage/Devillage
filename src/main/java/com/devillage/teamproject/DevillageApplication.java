f8f8c24382b5c0df7bf6a43e6997e4f6c510bb40
yangddoddi <pud1251@gmail.com>
2022-09-19 07:36:04 +0900

    Feat: 초기 설정
    SpringBoot2.7.3, Gradle1.0.10, springDataJPA, SpringOauth2Client, SpringSecurity, SpringValidation, Spring web,
    Lombok, h2, mysqlConnector, querydsl
    만일을 대비해 querydsl도 설치했으나 사용 시 협의 필요.

---
+++ src/main/java/com/devillage/teamproject/DevillageApplication.java
@@ -0,0 +1,13 @@
+package com.devillage.teamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevillageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevillageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devillage/teamproject/DevillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devillage.teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevillageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

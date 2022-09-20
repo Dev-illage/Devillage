6be9ae0518d2e7857d1f003b7620171bd2ee42f5
Qone2 <whrbdnjs33@naver.com>
2022-09-20 22:45:52 +0900

    Feat : User Controller getProfile() 메소드 구현

--- src/main/java/com/devillage/teamproject/controller/user/UserControllerImpl.java
+++ src/main/java/com/devillage/teamproject/controller/user/UserControllerImpl.java
@@ -1,10 +1,17 @@
 package com.devillage.teamproject.controller.user;
 
 import com.devillage.teamproject.dto.UserDto;
+import com.devillage.teamproject.service.user.UserService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserControllerImpl implements UserController {
+    private final UserService userService;
+
+    public UserControllerImpl(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public Long postBlock(Long id) {
         return null;
@@ -22,7 +29,7 @@ public class UserControllerImpl implements UserController {
 
     @Override
     public UserDto.Response getProfile(Long id) {
-        return null;
+        return UserDto.Response.of(userService.findUser(id));
     }
 
     @Override

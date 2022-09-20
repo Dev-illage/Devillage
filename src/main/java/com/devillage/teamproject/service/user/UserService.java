ec5a4f2f2d0424ecc3dcf332b45065091e57eb4f
Qone2 <whrbdnjs33@naver.com>
2022-09-20 22:18:46 +0900

    Feat : @Component 어노테이션 위치 변경

--- src/main/java/com/devillage/teamproject/service/user/UserService.java
+++ src/main/java/com/devillage/teamproject/service/user/UserService.java
@@ -1,11 +1,9 @@
 package com.devillage.teamproject.service.user;
 
 import com.devillage.teamproject.entity.User;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface UserService {
     User joinUser(User user);
 
--- src/main/java/com/devillage/teamproject/service/user/UserServiceImpl.java
+++ src/main/java/com/devillage/teamproject/service/user/UserServiceImpl.java
@@ -4,10 +4,12 @@ import com.devillage.teamproject.entity.User;
 import com.devillage.teamproject.exception.BusinessLogicException;
 import com.devillage.teamproject.exception.ExceptionCode;
 import com.devillage.teamproject.repository.user.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;

7dbafcf9af7fb327520ecd2dd8f23c77604370e5
kkg5 <kkg05102@gmail.com>
2022-09-22 18:30:13 +0900

    Test: PostService Test 작성

---
+++ src/test/java/com/devillage/teamproject/service/post/LikeTest.java
@@ -0,0 +1,90 @@
+package com.devillage.teamproject.service.post;
+
+import com.devillage.teamproject.entity.Like;
+import com.devillage.teamproject.entity.Post;
+import com.devillage.teamproject.entity.User;
+import com.devillage.teamproject.repository.post.LikeRepository;
+import com.devillage.teamproject.repository.post.PostRepository;
+import com.devillage.teamproject.repository.user.UserRepository;
+import com.devillage.teamproject.util.Reflection;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class LikeTest implements Reflection {
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private LikeRepository likeRepository;
+
+    @InjectMocks
+    private PostServiceImpl postService;
+
+    User user = newInstance(User.class);
+    Post post = newInstance(Post.class);
+    Long userId = 1L; // Security 메서드 구현 필요
+    Long postId = 1L;
+
+    LikeTest() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        setField(user, "likes", new ArrayList<>());
+    }
+
+    ;
+
+
+    @Test
+    void createLike() {
+        // given
+        given(userRepository.findById(userId))
+                .willReturn(Optional.of(user));
+        given(postRepository.findById(postId))
+                .willReturn(Optional.of(post));
+
+        given(likeRepository.findByUserIdAndPostId(userId, postId))
+                .willReturn(new ArrayList<>());
+
+        // when
+        Like findLike = postService.postLike(postId);
+
+        // then
+        Assertions.assertThat(findLike.getPost()).isEqualTo(post);
+        Assertions.assertThat(findLike.getUser()).isEqualTo(user);
+    }
+
+    @Test
+    void deleteLike() {
+        // given
+        Like like = new Like(user, post);
+
+        given(userRepository.findById(userId))
+                .willReturn(Optional.of(user));
+        given(postRepository.findById(postId))
+                .willReturn(Optional.of(post));
+
+        given(likeRepository.findByUserIdAndPostId(userId, postId))
+                .willReturn(List.of(like));
+
+        // when
+        Like findLike = postService.postLike(postId);
+
+        // then
+        Assertions.assertThat(findLike).isEqualTo(like);
+    }
+
+}
---
+++ src/test/java/com/devillage/teamproject/service/post/ReportTest.java
@@ -0,0 +1,87 @@
+package com.devillage.teamproject.service.post;
+
+import com.devillage.teamproject.entity.Post;
+import com.devillage.teamproject.entity.ReportedPost;
+import com.devillage.teamproject.entity.User;
+import com.devillage.teamproject.exception.BusinessLogicException;
+import com.devillage.teamproject.repository.post.PostRepository;
+import com.devillage.teamproject.repository.post.ReportedPostRepository;
+import com.devillage.teamproject.repository.user.UserRepository;
+import com.devillage.teamproject.util.Reflection;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class ReportTest implements Reflection {
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ReportedPostRepository reportRepository;
+
+    @InjectMocks
+    private PostServiceImpl postService;
+
+    User user = newInstance(User.class);
+    Post post = newInstance(Post.class);
+    Long userId = 1L; // Security 메서드 구현 필요
+    Long postId = 1L;
+
+    ReportTest() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        setField(user, "reportedPosts", new ArrayList<>());
+    }
+
+    @Test
+    void createReportedPost() {
+        // given
+        given(userRepository.findById(userId))
+                .willReturn(Optional.of(user));
+        given(postRepository.findById(postId))
+                .willReturn(Optional.of(post));
+
+        given(reportRepository.findByUserIdAndPostId(userId, postId))
+                .willReturn(new ArrayList<>());
+
+        // when
+        ReportedPost findReportedPost = postService.postReport(postId);
+
+        // then
+        Assertions.assertThat(findReportedPost.getPost()).isEqualTo(post);
+        Assertions.assertThat(findReportedPost.getUser()).isEqualTo(user);
+    }
+
+    @Test
+    void alreadyReportPost() {
+        // given
+        ReportedPost report = new ReportedPost(user, post);
+
+        given(userRepository.findById(userId))
+                .willReturn(Optional.of(user));
+        given(postRepository.findById(postId))
+                .willReturn(Optional.of(post));
+
+        given(reportRepository.findByUserIdAndPostId(userId, postId))
+                .willReturn(List.of(report));
+
+        // when / then
+        assertThrows(BusinessLogicException.class,
+                () -> postService.postReport(postId));
+    }
+
+}

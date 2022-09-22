794b85a50be7227100a212aab3b081d566bc1b19
yangddoddi <pud1251@gmail.com>
2022-09-22 15:46:19 +0900

    Test : 로그인 기능 테스트 및 테스트 편의성 유틸리티 클래스 추가
    DTO 누락으로 인해 급하게 PR하느라 실패 테스트는 아직 수행하지 못했습니다. 추후 보강 예정

--- src/test/java/com/devillage/teamproject/service/auth/AuthServiceTest.java
+++ src/test/java/com/devillage/teamproject/service/auth/AuthServiceTest.java
@@ -1,10 +1,15 @@
-package com.devillage.teamproject.service;
+package com.devillage.teamproject.service.auth;
 
+import com.devillage.teamproject.dto.AuthDto;
+import com.devillage.teamproject.entity.Role;
 import com.devillage.teamproject.entity.User;
+import com.devillage.teamproject.entity.UserRoles;
+import com.devillage.teamproject.entity.enums.RoleType;
 import com.devillage.teamproject.repository.user.UserRepository;
-import com.devillage.teamproject.security.config.PasswordEncoderConfig;
-import com.devillage.teamproject.service.auth.AuthServiceImpl;
-import com.devillage.teamproject.util.Reflection;
+import com.devillage.teamproject.security.util.JwtTokenUtil;
+import com.devillage.teamproject.util.ReflectionForStatic;
+import com.devillage.teamproject.util.TestConfig;
+import com.devillage.teamproject.util.auth.AuthTestUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,29 +18,41 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import static com.devillage.teamproject.util.ReflectionForStatic.*;
+import static com.devillage.teamproject.util.TestConstants.*;
+import static com.devillage.teamproject.util.auth.AuthTestUtils.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @Slf4j
+@Import({TestConfig.class})
 @ExtendWith(MockitoExtension.class)
-public class AuthServiceTest implements Reflection {
+public class AuthServiceTest implements ReflectionForStatic {
     @Mock
     private UserRepository userRepository;
 
-    @Mock
-    private PasswordEncoder passwordEncoder;
+    @InjectMocks
+    private BCryptPasswordEncoder passwordEncoder;
 
     @InjectMocks
     private AuthServiceImpl authService;
+
+    @Mock
+    private JwtTokenUtil jwtTokenUtil;
 
     @BeforeEach
     public void injectBean() throws NoSuchFieldException, IllegalAccessException {
@@ -46,23 +63,46 @@ public class AuthServiceTest implements Reflection {
     @DisplayName("joinUser Success test")
     public void test1() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, NoSuchFieldException {
         // given
-        String email = "email@test.com";
-        String nickName = "jamjamTest";
-        String password = "testPassword123!";
-
-        User user = newInstance(User.class);
-        setField(user, "email", email);
-        setField(user, "nickName", nickName);
-        setField(user, "password", password);
+        User user = createTestUser(EMAIL1, NICKNAME1, PASSWORD1);
 
         given(userRepository.save(any(User.class))).willReturn(user);
 
         // when
-
         User savedUser = authService.joinUser(user);
 
-        Assertions.assertThat(savedUser.getPassword()).isEqualTo(user.getPassword());
-        Assertions.assertThat(savedUser.getEmail()).isEqualTo(user.getEmail());
-        Assertions.assertThat(savedUser.getNickName()).isEqualTo(user.getNickName());
+        assertThat(savedUser.getPassword()).isEqualTo(user.getPassword());
+        assertThat(savedUser.getEmail()).isEqualTo(user.getEmail());
+        assertThat(savedUser.getNickName()).isEqualTo(user.getNickName());
+    }
+
+    @Test
+    @DisplayName("loginUser Success test")
+    public void test2() throws NoSuchFieldException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        //given
+        User user = createTestUser(EMAIL1, PASSWORD1);
+
+        String accessToken = createToken(EMAIL1, ROLES, SECRET_EXPIRE, SECRET_KEY, ID1);
+        String refreshToken = createRefreshToken(EMAIL1, ROLES, SECRET_EXPIRE, REFRESH_KEY);
+
+        User savedUser = createTestUser(EMAIL1, passwordEncoder.encode(PASSWORD1));
+        setField(savedUser, "id", ID1);
+
+        Role role = newInstance(Role.class);
+        setField(role, "roleType", RoleType.ROLE_USER);
+        UserRoles userRoles = new UserRoles(role, savedUser);
+        List<UserRoles> userRolesList = List.of(userRoles);
+        setField(savedUser, "userRoles", userRolesList);
+
+        given(userRepository.findUserByEmail(EMAIL1)).willReturn(Optional.of(savedUser));
+        given(jwtTokenUtil.createAccessToken(savedUser.getEmail(),savedUser.getId(),ROLES)).willReturn(accessToken);
+        given(jwtTokenUtil.createRefreshToken(savedUser.getEmail())).willReturn(refreshToken);
+
+        // when
+        AuthDto.Token token = authService.loginUser(user);
+
+        // then
+        assertThat(token.getBearer()).isEqualTo(BEARER);
+        assertThat(token.getAccessToken()).isEqualTo(accessToken);
+        assertThat(token.getRefreshToken()).isEqualTo(refreshToken);
     }
 }
---
+++ src/test/java/com/devillage/teamproject/util/TestConstants.java
@@ -0,0 +1,29 @@
+package com.devillage.teamproject.util;
+
+/*
+*
+* 메서드로 빼기는 애매하고 별도 클래스 만들기도 애매한 친구들은
+* 여기 상수로 지정해놓고 필요할 때 찾아쓰면 편할 것 같아요
+*
+* */
+
+import java.util.Collections;
+import java.util.List;
+
+public class TestConstants {
+    public static String SECRET_KEY = "xptmxmdlsepeocndgkrpwhagownwldjfakskrlfrpTmfkrh";
+    public static String REFRESH_KEY = "dhkwlsWktlagkekdlfjgrpaksgdlTjdigksekrhwlsWkfhdlrpakwsi";
+    public final static String BEARER = "Bearer ";
+    public final static Long SECRET_EXPIRE = 600000L;
+    public final static Long REFRESH_EXPIRE = 604800000L;
+    public final static String EMAIL1 = "email@test.com";
+    public final static String PASSWORD1 = "testPassword!23";
+    public final static String NICKNAME1 = "코딩잘하고싶다1";
+    public final static Long ID1 = 1L;
+    public final static String EMAIL2 = "email2@test.com";
+    public final static String PASSWORD2 = "testPassword!23";
+    public final static String NICKNAME2 = "코딩잘하고싶다2";
+    public final static Long ID2 = 2L;
+    public final static Long WRONG_ID = 50000L;
+    public final static List<String> ROLES = Collections.singletonList("ROLE_USER");
+}

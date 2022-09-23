584525f71de25c771a778885394690d0f70e8c1d
yangddoddi <pud1251@gmail.com>
2022-09-23 22:06:53 +0900

    Fix : User와 RefreshToken 연관관계 매핑 해제
    전체적인 로그인 로직 변경으로 인해 연관관계 매핑을 제거했습니다.

--- src/main/java/com/devillage/teamproject/entity/User.java
+++ src/main/java/com/devillage/teamproject/entity/User.java
@@ -62,9 +62,6 @@ public class User extends AuditingEntity {
     @ToString.Include
     private String oauthProvider;
 
-    @OneToOne(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
-    private RefreshToken refreshToken;
-
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Bookmark> bookmarks = new ArrayList<>();
 
@@ -105,19 +102,11 @@ public class User extends AuditingEntity {
         this.nickName = nickName;
     }
 
-    public void addPost(Post post) {
-        posts.add(post);
-    }
-
     public void passwordEncryption(PasswordEncoder passwordEncoder) {
         this.password = passwordEncoder.encode(this.password);
     }
 
     public boolean passwordVerification(PasswordEncoder passwordEncoder, User user) {
         return passwordEncoder.matches(user.getPassword(), this.getPassword());
-    }
-
-    public void addRefreshToken(String token) {
-        this.refreshToken = new RefreshToken(token, this);
     }
 }

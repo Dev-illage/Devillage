c2e7f184c55acff4bead1e4c8d86e6353ff8133b
kkg5 <kkg05102@gmail.com>
2022-09-24 00:13:49 +0900

    Feat: PostRepository findByCategory_CategoryType 메서드 추가

--- src/main/java/com/devillage/teamproject/repository/post/PostRepository.java
+++ src/main/java/com/devillage/teamproject/repository/post/PostRepository.java
@@ -1,7 +1,11 @@
 package com.devillage.teamproject.repository.post;
 
 import com.devillage.teamproject.entity.Post;
+import com.devillage.teamproject.entity.enums.CategoryType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByCategory_CategoryType(CategoryType categoryType, Pageable pageable);
 }

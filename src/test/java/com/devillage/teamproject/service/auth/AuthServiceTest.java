904c0c468147735870b06e9a283887873255c336
yangddoddi <pud1251@gmail.com>
2022-09-23 22:18:55 +0900

    Test : 토큰 재발행 로직 3계층 테스트 및 테스트 유틸리티 클래스 작성
    기능성 클래스에 대한 테스트는 포함되지 않았습니다.

--- src/test/java/com/devillage/teamproject/service/auth/AuthServiceTest.java
+++ src/test/java/com/devillage/teamproject/service/auth/AuthServiceTest.java
@@ -1,42 +1,47 @@
 package com.devillage.teamproject.service.auth;
 
 import com.devillage.teamproject.dto.AuthDto;
+import com.devillage.teamproject.entity.RefreshToken;
 import com.devillage.teamproject.entity.Role;
 import com.devillage.teamproject.entity.User;
 import com.devillage.teamproject.entity.UserRoles;
 import com.devillage.teamproject.entity.enums.RoleType;
+import com.devillage.teamproject.repository.role.RoleRepository;
+import com.devillage.teamproject.repository.token.RefreshTokenRepository;
 import com.devillage.teamproject.repository.user.UserRepository;
+import com.devillage.teamproject.repository.user_roles.UserRolesRepository;
+import com.devillage.teamproject.security.util.JwtConstants;
 import com.devillage.teamproject.security.util.JwtTokenUtil;
 import com.devillage.teamproject.util.ReflectionForStatic;
 import com.devillage.teamproject.util.TestConfig;
 import com.devillage.teamproject.util.auth.AuthTestUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
+import static com.devillage.teamproject.security.util.JwtConstants.*;
 import static com.devillage.teamproject.util.ReflectionForStatic.*;
 import static com.devillage.teamproject.util.TestConstants.*;
+import static com.devillage.teamproject.util.TestConstants.ROLES;
+import static com.devillage.teamproject.util.TestConstants.SECRET_KEY;
 import static com.devillage.teamproject.util.auth.AuthTestUtils.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
 
 @Slf4j
 @Import({TestConfig.class})
@@ -54,6 +59,15 @@ public class AuthServiceTest implements ReflectionForStatic {
     @Mock
     private JwtTokenUtil jwtTokenUtil;
 
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
+    private UserRolesRepository userRolesRepository;
+
+    @Mock
+    private RefreshTokenRepository refreshTokenRepository;
+
     @BeforeEach
     public void injectBean() throws NoSuchFieldException, IllegalAccessException {
         setField(authService, "passwordEncoder", passwordEncoder);
@@ -64,8 +78,13 @@ public class AuthServiceTest implements ReflectionForStatic {
     public void test1() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, NoSuchFieldException {
         // given
         User user = createTestUser(EMAIL1, NICKNAME1, PASSWORD1);
+        Role role = newInstance(Role.class);
+        setField(role,"roleType",RoleType.ROLE_USER);
+        UserRoles userRoles = new UserRoles(role, user);
 
         given(userRepository.save(any(User.class))).willReturn(user);
+        given(roleRepository.getRoleByRoleType(any(RoleType.class))).willReturn(role);
+        given(userRolesRepository.save(userRoles)).willReturn(userRoles);
 
         // when
         User savedUser = authService.joinUser(user);
@@ -81,8 +100,8 @@ public class AuthServiceTest implements ReflectionForStatic {
         //given
         User user = createTestUser(EMAIL1, PASSWORD1);
 
-        String accessToken = createToken(EMAIL1, ROLES, SECRET_EXPIRE, SECRET_KEY, ID1);
-        String refreshToken = createRefreshToken(EMAIL1, ROLES, SECRET_EXPIRE, REFRESH_KEY);
+        String accessToken = createToken(EMAIL1,  ID1, ROLES, SECRET_KEY.getBytes(), SECRET_EXPIRE);
+        String refreshToken = createToken(EMAIL1,  ID1, ROLES, REFRESH_KEY.getBytes(), SECRET_EXPIRE);
 
         User savedUser = createTestUser(EMAIL1, passwordEncoder.encode(PASSWORD1));
         setField(savedUser, "id", ID1);
@@ -95,7 +114,7 @@ public class AuthServiceTest implements ReflectionForStatic {
 
         given(userRepository.findUserByEmail(EMAIL1)).willReturn(Optional.of(savedUser));
         given(jwtTokenUtil.createAccessToken(savedUser.getEmail(),savedUser.getId(),ROLES)).willReturn(accessToken);
-        given(jwtTokenUtil.createRefreshToken(savedUser.getEmail())).willReturn(refreshToken);
+        given(jwtTokenUtil.createRefreshToken(savedUser.getEmail(), savedUser.getId(),ROLES)).willReturn(refreshToken);
 
         // when
         AuthDto.Token token = authService.loginUser(user);
@@ -104,5 +123,35 @@ public class AuthServiceTest implements ReflectionForStatic {
         assertThat(token.getBearer()).isEqualTo(BEARER);
         assertThat(token.getAccessToken()).isEqualTo(accessToken);
         assertThat(token.getRefreshToken()).isEqualTo(refreshToken);
+    }
+
+    @Test
+    @DisplayName("tokenReissue success test")
+    public void test3() throws NoSuchFieldException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        // given
+        String splitToken = createToken(EMAIL1,  ID1, ROLES, REFRESH_KEY.getBytes(), SECRET_EXPIRE);
+        String request = BEARER + splitToken;
+        RefreshToken existingToken = new RefreshToken(splitToken);
+
+        Claims claims = Jwts.claims()
+                        .setSubject(EMAIL1);
+        claims.put(SEQUENCE, ID1);
+        claims.put(JwtConstants.ROLES, ROLES);
+
+        given(refreshTokenRepository.findRefreshTokenByTokenValue(anyString())).willReturn(Optional.of(existingToken));
+        given(jwtTokenUtil.parseRefreshToken(splitToken)).willReturn(claims);
+        given(jwtTokenUtil.createAccessToken(EMAIL1, ID1, ROLES)).willReturn(createToken(EMAIL1, ID1, ROLES, SECRET_KEY.getBytes(), SECRET_EXPIRE));
+        given(jwtTokenUtil.createRefreshToken(EMAIL1, ID1, ROLES)).willReturn(createToken(EMAIL1, ID1, ROLES, REFRESH_KEY.getBytes(), SECRET_EXPIRE));
+
+
+        // when
+        AuthDto.Token token = authService.reIssue(request);
+
+        String accessTokenSubject = AuthTestUtils.parseToken(token.getAccessToken(), SECRET_KEY.getBytes()).getSubject();
+        String refreshTokenSubject = AuthTestUtils.parseToken(token.getRefreshToken(), REFRESH_KEY.getBytes()).getSubject();
+
+        // then
+        assertThat(accessTokenSubject).isEqualTo(EMAIL1);
+        assertThat(refreshTokenSubject).isEqualTo(EMAIL1);
     }
 }

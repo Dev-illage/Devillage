ae941de337d72b8333c1e82e07a079b015f63fdc
gangdodan <dodannnn4@gmail.com>
2022-09-25 13:49:42 +0900

    Test: PostControllerTest > getPost 테스트 작성 및 postPost 테스트 일부 수정

--- src/test/java/com/devillage/teamproject/controller/post/PostControllerTest.java
+++ src/test/java/com/devillage/teamproject/controller/post/PostControllerTest.java
@@ -1,7 +1,9 @@
 package com.devillage.teamproject.controller.post;
 
 import com.devillage.teamproject.dto.PostDto;
+import com.devillage.teamproject.dto.UserDto;
 import com.devillage.teamproject.entity.*;
+import com.devillage.teamproject.entity.enums.CategoryType;
 import com.devillage.teamproject.service.post.PostService;
 import com.devillage.teamproject.util.Reflection;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,20 +17,27 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+
 
 import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import static com.devillage.teamproject.entity.enums.CategoryType.FREE;
+import static java.nio.file.Paths.get;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -56,18 +65,28 @@ class PostControllerTest implements Reflection {
         setField(user, "id", 1L);
         setField(post, "id", 2L);
     }
-
+    @WithMockUser
     @Test
     public void postPost() throws Exception{
         //given
-        PostDto.Post post = newInstance(PostDto.Post.class);
-        setField(post,"title","Mockito 관련 질문입니다.");
-        setField(post,"content", "안녕하세요. 스트링 통째로 드가는게 맞나요");
+        PostDto.Post postDto = newInstance(PostDto.Post.class);
+        Category category = newInstance(Category.class);
+        PostTag postTag = newInstance(PostTag.class);
+        Tag tag = newInstance(Tag.class);
 
-        Post convertEntity = PostDto.Post.toEntity(post);
-        given(postService.savePost(Mockito.any(Post.class))).willReturn(convertEntity);
+        setField(postDto,"category",category);
+        setField(postDto,"title","Mockito 관련 질문입니다.");
+        setField(postDto,"tags",List.of(postTag));
+        setField(postDto,"content", "안녕하세요. 스트링 통째로 드가는게 맞나요");
+        setField(category,"categoryType",CategoryType.NOTICE);
+        setField(postTag, "tag", tag);
+        setField(tag, "id", 1L);
+        setField(tag,"name", "mvcTest");
+
+        given(postService.savePost(Mockito.any(Post.class))).willReturn(post);
 
         String content = objectMapper.writeValueAsString(post);
+
 
         //when
         ResultActions actions =
@@ -79,13 +98,69 @@ class PostControllerTest implements Reflection {
                 );
 
         //then
+        //todo: 검증 조건 및 restdocs 추가 예정
         MvcResult result = actions
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.data.title").value(post.getTitle()))
                 .andExpect(jsonPath("$.data.content").value(post.getContent()))
                 .andReturn();
+
     }
 
+    @WithMockUser
+    @Test
+    void getPost() throws Exception{
+        //given
+        UserDto.AuthorInfo authorInfo = newInstance(UserDto.AuthorInfo.class);
+        Category category = newInstance(Category.class);
+        PostTag postTag = newInstance(PostTag.class);
+        Tag tag = newInstance(Tag.class);
+        Comment comment = newInstance(Comment.class);
+
+        setField(post,"category",category);
+        setField(post,"id",1L);
+        setField(post,"title","Mockito 관련 질문입니다.");
+        setField(post,"tags",List.of(postTag));
+        setField(post,"content", "안녕하세요. 스트링 통째로 드가는게 맞나요");
+        setField(post, "clicks", 1L);
+        setField(post, "createdAt", LocalDateTime.now());
+        setField(post, "lastModifiedAt", LocalDateTime.now());
+        setField(category,"categoryType",CategoryType.NOTICE);
+        setField(postTag, "tag", tag);
+        setField(tag, "id", 1L);
+        setField(tag,"name", "mvcTest");
+        setField(comment,"id",1L);
+        setField(comment,"content","잘 봤습니다.");
+        setField(authorInfo,"authorId",1L);
+        setField(authorInfo,"authorName","강지");
+        Long id = post.getId();
+
+        given(postService.getPost(Mockito.any(long.class))).willReturn(post);
+
+        //when
+        ResultActions actions =
+                mockMvc.perform(
+                        get("/posts/{post-id}",id)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                );
+
+        //then
+        //todo: 검증 조건 및 restdocs 추가 예정
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response.title").value(post.getTitle()))
+                .andExpect(jsonPath("$.response.content").value(post.getContent()))
+                .andReturn();
+
+
+    }
+
+
+    @Test
+    public void pathPost() throws Exception{
+
+    }
 
     @Test
     void postBookmark() throws Exception {
@@ -119,6 +194,8 @@ class PostControllerTest implements Reflection {
                                 fieldWithPath("data.bookmark").type(JsonFieldType.NUMBER).description("북마크 식별자")
                         )
                 ));
+
+
 
     }
 

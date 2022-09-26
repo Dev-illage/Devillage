0c86dc73417e453584e28ee8619a8d0902caf568
Qone2 <whrbdnjs33@naver.com>
2022-09-26 13:46:41 +0900

    Fix : 빠졌던 Transactional추가

--- src/main/java/com/devillage/teamproject/service/user/UserServiceImpl.java
+++ src/main/java/com/devillage/teamproject/service/user/UserServiceImpl.java
@@ -8,11 +8,13 @@ import com.devillage.teamproject.repository.user.BlockRepository;
 import com.devillage.teamproject.repository.user.UserRepository;
 import com.devillage.teamproject.security.util.JwtTokenUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -26,6 +28,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public User joinUser(User user) {
         return null;
     }
@@ -37,6 +40,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public User editUser(User user) {
         return null;
     }
@@ -47,12 +51,14 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void deleteUser(String token) {
         User findUser = findUser(jwtTokenUtil.getUserId(token));
         findUser.deleteUser();
     }
 
     @Override
+    @Transactional
     public Block blockUser(Long destUserId, String token) {
         Long srcUserId = jwtTokenUtil.getUserId(token);
         Optional<Block> optionalBlock = blockRepository.findBySrcUserIdAndDestUserId(srcUserId, destUserId);

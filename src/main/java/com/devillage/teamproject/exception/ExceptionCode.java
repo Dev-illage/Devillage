2403c3e3ddd41e4e04d6f83ec5152d2470579a94
kkg5 <kkg05102@gmail.com>
2022-09-26 23:33:30 +0900

    Feat: ExceptionCode 예외 추가 & 순서 변경 COMMENT_NOT_FOUND, RE_COMMENT_NOT_FOUND, ID_DOES_NOT_MATCH 추가

--- src/main/java/com/devillage/teamproject/exception/ExceptionCode.java
+++ src/main/java/com/devillage/teamproject/exception/ExceptionCode.java
@@ -4,17 +4,25 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionCode {
-    USER_NOT_FOUND(404, "User not found"),
-    EXISTING_USER(409, "Existing User"),
-    POST_NOT_FOUND(404, "Post not found"),
-    ALREADY_REPORTED(400, "Already report"),
     MALFORMED_JWT_EXCEPTION(401, "Malformed Jwt Exception"),
     UNSUPPORTED_JWT_EXCEPTION(401, "Unsupported Jwt Exception"),
     SIGNATURE_EXCEPTION(401, "Signature Exception"),
     EXPIRED_JWT_EXCEPTION(401, "Expired Jwt Exception"),
+
+    USER_NOT_FOUND(404, "User not found"),
+    POST_NOT_FOUND(404, "Post not found"),
     CATEGORY_NOT_FOUND(404, "Category not found"),
-    USER_RESIGNED(422, "Resigned User"),
+    COMMENT_NOT_FOUND(404, "Comment not found"),
+    RE_COMMENT_NOT_FOUND(404, "ReComment not found"),
+
+    EXISTING_USER(409, "Existing User"),
+    ALREADY_REPORTED(409, "Already reported"),
+    ID_DOES_NOT_MATCH(409, "Id does not match"),
+
+    USER_RESIGNED(422, "Resigned User")
+
     ;
+
     private final int status;
     private final String message;
 

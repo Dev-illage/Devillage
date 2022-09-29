0f8e7c9ce8bae0fa9c5c578b41283ec424b28717
yangddoddi <pud1251@gmail.com>
2022-09-29 23:41:06 +0900

    Fix : 깃허브 가입 시 이메일을 저장하지 못했던 문제 해결
    깃허브 가입 시에는 이메일을 받아오지 못해서(유저가 허용하지 않을 시) 임의 계정을 만들어 가입시ㅣ키는 것으로 변경하였습니다.

--- src/main/java/com/devillage/teamproject/security/oauth/CustomSuccessHandler.java
+++ src/main/java/com/devillage/teamproject/security/oauth/CustomSuccessHandler.java
@@ -7,10 +7,13 @@ import com.devillage.teamproject.exception.BusinessLogicException;
 import com.devillage.teamproject.exception.ExceptionCode;
 import com.devillage.teamproject.repository.token.RefreshTokenRepository;
 import com.devillage.teamproject.repository.user.UserRepository;
+import com.devillage.teamproject.security.util.JwtConstants;
 import com.devillage.teamproject.security.util.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -21,6 +24,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+
+import static com.devillage.teamproject.security.util.JwtConstants.*;
 
 
 @RequiredArgsConstructor
@@ -39,7 +44,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         }
 
         OAuth2User user = (OAuth2User) authentication.getPrincipal();
-        String email = user.getAttributes().get("email") == null ? (String)user.getAttributes().get("url") : (String) user.getAttributes().get("email");
+        String email = user.getAttributes().get("email") == null ? makeEmail(user) : (String) user.getAttributes().get("email");
 
         User findUser = userRepository.findUserByEmail(email).orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
 
@@ -48,13 +53,25 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         refreshTokenRepository.save(new RefreshToken(refreshToken));
 
-        String token = UriComponentsBuilder.fromUriString("http://localhost:3000/login/oauth")
+        String url = getURL(accessToken, refreshToken);
+
+        getRedirectStrategy().sendRedirect(request, response, url);
+    }
+
+    private String getURL(String accessToken, String refreshToken) {
+        return UriComponentsBuilder.fromUriString(REDIRECT_URL_OAUTH2)
                 .queryParam("accessToken", accessToken)
                 .queryParam("refreshToken", refreshToken)
                 .build()
                 .toUriString();
+    }
 
-        getRedirectStrategy().sendRedirect(request, response, token);
+    private String makeEmail(OAuth2User user) {
+        Integer id =  (Integer)user.getAttributes().get("id");
+        String provider = "github";
+        String url = "@github.com";
+
+        return id+provider+url;
     }
 
 }

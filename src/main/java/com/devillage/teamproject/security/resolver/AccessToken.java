bc2da79e2f9d8a568b894579e69df162839a35c0
yangddoddi <pud1251@gmail.com>
2022-09-30 08:02:07 +0900

    Feat : Token에서 유저 정보를 바로 가져올 수 있도록 어노테이션 추가

---
+++ src/main/java/com/devillage/teamproject/security/resolver/AccessToken.java
@@ -0,0 +1,12 @@
+package com.devillage.teamproject.security.resolver;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+@Target(PARAMETER)
+@Retention(RUNTIME)
+public @interface AccessToken {
+}
---
+++ src/main/java/com/devillage/teamproject/security/resolver/ResultJwtArgumentResolver.java
@@ -0,0 +1,54 @@
+package com.devillage.teamproject.security.resolver;
+
+import com.devillage.teamproject.dto.AuthDto;
+import com.devillage.teamproject.exception.BusinessLogicException;
+import com.devillage.teamproject.security.util.JwtTokenUtil;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.List;
+
+import static com.devillage.teamproject.exception.ExceptionCode.MALFORMED_JWT_EXCEPTION;
+import static com.devillage.teamproject.security.util.JwtConstants.*;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ResultJwtArgumentResolver implements HandlerMethodArgumentResolver {
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(AccessToken.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        log.info("시작");
+        String accessToken = webRequest.getHeader(AUTHORIZATION_HEADER);
+        log.info("accessToken = {}", accessToken);
+
+        if ( accessToken==null ) {
+            throw new BusinessLogicException(MALFORMED_JWT_EXCEPTION);
+        }
+
+        String token = jwtTokenUtil.splitToken(accessToken);
+        Claims claims = jwtTokenUtil.paresAccessToken(token);
+        String email = claims.getSubject();
+        List<String> roles = (List) claims.get(ROLES);
+        Long id = Long.valueOf((Integer)claims.get(SEQUENCE));
+
+        return AuthDto.UserInfo.builder()
+                .id(id)
+                .email(email)
+                .roles(roles)
+                .build();
+    }
+}

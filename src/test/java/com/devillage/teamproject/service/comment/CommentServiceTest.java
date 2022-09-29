efb086fca2cd4796e2cb77ca8fc8ba36a77d5733
Qone2 <whrbdnjs33@naver.com>
2022-09-29 21:43:11 +0900

    Test : 댓글 삭제 서비스 테스트

--- src/test/java/com/devillage/teamproject/service/comment/CommentServiceTest.java
+++ src/test/java/com/devillage/teamproject/service/comment/CommentServiceTest.java
@@ -4,6 +4,8 @@ import com.devillage.teamproject.entity.Comment;
 import com.devillage.teamproject.entity.Post;
 import com.devillage.teamproject.entity.ReComment;
 import com.devillage.teamproject.entity.User;
+import com.devillage.teamproject.entity.enums.CommentStatus;
+import com.devillage.teamproject.exception.BusinessLogicException;
 import com.devillage.teamproject.repository.comment.CommentRepository;
 import com.devillage.teamproject.repository.comment.ReCommentRepository;
 import com.devillage.teamproject.security.util.JwtTokenUtil;
@@ -89,6 +91,45 @@ class CommentServiceTest {
         assertEquals(reCommentDto.getContent(), actualReComment.getContent());
         assertEquals(user, actualReComment.getUser());
         assertEquals(comment, actualReComment.getComment());
+    }
+
+    /**
+     * 일반 삭제는 단위테스트로 검증하기엔 불가능해 보입니다.
+     */
+    @Test
+    @DisplayName("대댓글이 있는 상태에서의 삭제")
+    public void deleteCommentWithReComment() throws Exception {
+        // given
+        User user = User.builder().id(ID1).build();
+        Comment comment = Comment.builder().id(ID1).content(COMMENT_CONTENT).user(user).build();
+        ReComment reComment = ReComment.builder().id(ID1).comment(comment).build();
+        comment.getReComments().add(reComment);
+
+        given(commentRepository.findById(Mockito.anyLong())).willReturn(Optional.of(comment));
+        given(jwtTokenUtil.getUserId(Mockito.anyString())).willReturn(user.getId());
+
+        // when
+        commentService.deleteComment(comment.getId(), "someToken");
+
+        // then
+        assertEquals(CommentStatus.DELETED, comment.getCommentStatus());
+    }
+
+    @Test
+    @DisplayName("유저가 일치하지 않으면 삭제가 불가능")
+    public void deleteComment() throws Exception {
+        // given
+        User user = User.builder().id(ID1).build();
+        Comment comment = Comment.builder().id(ID1).content(COMMENT_CONTENT).user(user).build();
+        ReComment reComment = ReComment.builder().id(ID1).comment(comment).build();
+        comment.getReComments().add(reComment);
+
+        given(commentRepository.findById(Mockito.anyLong())).willReturn(Optional.of(comment));
+        given(jwtTokenUtil.getUserId(Mockito.anyString())).willReturn(user.getId() + 1);
+
+        // when then
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.deleteComment(comment.getId(), "someToken"));
     }
 
 }

16f58fcf278b75da96d4f3687e28b60f3b8f7a63
kkg5 <kkg05102@gmail.com>
2022-09-29 23:44:35 +0900

    Feat: CommentController > patchComment/patchReComment 작성

--- src/main/java/com/devillage/teamproject/controller/comment/CommentController.java
+++ src/main/java/com/devillage/teamproject/controller/comment/CommentController.java
@@ -11,13 +11,13 @@ public interface CommentController {
     @PostMapping("/{post-id}/comments")
     @ResponseStatus(HttpStatus.CREATED)
     CommentDto.Response postComment(CommentDto.Post request, @PathVariable("post-id") Long postId,
-                     @RequestHeader(JwtConstants.AUTHORIZATION_HEADER) String token);
+                                    @RequestHeader(JwtConstants.AUTHORIZATION_HEADER) String token);
 
     @PostMapping("/{post-id}/comments/{comment-id}")
     @ResponseStatus(HttpStatus.CREATED)
     Long postReComment(@PathVariable("post-id") Long postId,
                        @PathVariable("comment-id") Long commentId,
-                       CommentDto.ReCommentPost request);
+                       @RequestBody CommentDto.ReCommentPost request);
 
     @PostMapping("/{post-id}/comments/{comment-id}/like")
     @ResponseStatus(HttpStatus.OK)
@@ -41,16 +41,16 @@ public interface CommentController {
 
     @PatchMapping("/{post-id}/comments/{comment-id}")
     @ResponseStatus(HttpStatus.OK)
-    Long patchComment(@PathVariable("post-id") Long postId,
-                      @PathVariable("comment-id") Long commentId,
-                      CommentDto.Patch request);
+    CommentDto.Response patchComment(@PathVariable("post-id") Long postId,
+                                     @PathVariable("comment-id") Long commentId,
+                                     @RequestBody CommentDto.Patch request);
 
     @PatchMapping("/{post-id}/comments/{comment-id}/{re-comment-id}")
     @ResponseStatus(HttpStatus.OK)
-    Long patchReComment(@PathVariable("post-id") Long postId,
-                        @PathVariable("comment-id") Long commentId,
-                        @PathVariable("re-comment-id") Long reCommentId,
-                        CommentDto.ReCommentPatch request);
+    CommentDto.ReCommentResponse patchReComment(@PathVariable("post-id") Long postId,
+                                                @PathVariable("comment-id") Long commentId,
+                                                @PathVariable("re-comment-id") Long reCommentId,
+                                                @RequestBody CommentDto.Patch request);
 
     @DeleteMapping("/{post-id}/comments/{comment-id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)

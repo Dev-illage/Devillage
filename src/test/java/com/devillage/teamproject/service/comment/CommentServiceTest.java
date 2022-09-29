188159af9028534bc3210f3c28c8bac25029bd19
kkg5 <kkg05102@gmail.com>
2022-09-29 23:47:12 +0900

    Test: patchComment/patchReComment 관련 테스트 작성

--- src/test/java/com/devillage/teamproject/service/comment/CommentServiceTest.java
+++ src/test/java/com/devillage/teamproject/service/comment/CommentServiceTest.java
@@ -2,12 +2,15 @@ package com.devillage.teamproject.service.comment;
 
 import com.devillage.teamproject.entity.Comment;
 import com.devillage.teamproject.entity.Post;
+import com.devillage.teamproject.entity.ReComment;
 import com.devillage.teamproject.entity.User;
+import com.devillage.teamproject.exception.BusinessLogicException;
 import com.devillage.teamproject.repository.comment.CommentRepository;
+import com.devillage.teamproject.repository.comment.ReCommentRepository;
 import com.devillage.teamproject.security.util.JwtTokenUtil;
 import com.devillage.teamproject.service.post.PostService;
 import com.devillage.teamproject.service.user.UserService;
-import com.devillage.teamproject.service.user.UserServiceImpl;
+import com.devillage.teamproject.util.Reflection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,18 +19,21 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
-import static com.devillage.teamproject.util.TestConstants.COMMENT_CONTENT;
-import static com.devillage.teamproject.util.TestConstants.ID1;
+import static com.devillage.teamproject.util.TestConstants.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 
 @ExtendWith(MockitoExtension.class)
-class CommentServiceTest {
+class CommentServiceTest implements Reflection {
     @Mock
     private CommentRepository commentRepository;
+    @Mock
+    private ReCommentRepository reCommentRepository;
     @Mock
     private JwtTokenUtil jwtTokenUtil;
     @Mock
@@ -36,6 +42,37 @@ class CommentServiceTest {
     private UserService userService;
     @InjectMocks
     private CommentServiceImpl commentService;
+
+    @Test
+    public void deleteReComment() throws Exception {
+        // given
+        Post post = newInstance(Post.class);
+        Comment comment = newInstance(Comment.class);
+        ReComment reComment = newInstance(ReComment.class);
+
+        setField(post, "id", 1L);
+        setField(comment, "id", 2L);
+        setField(reComment, "id", 3L);
+
+        setField(reComment, "comment", comment);
+        setField(comment, "post", post);
+
+        doNothing().when(reCommentRepository).deleteById(anyLong());
+        given(reCommentRepository.findById(reComment.getId()))
+                .willReturn(Optional.of(reComment));
+        given(reCommentRepository.findById(reComment.getId() + 1L))
+                .willReturn(Optional.empty());
+
+        // when / then
+        assertDoesNotThrow(
+                () -> commentService.deleteReComment(post.getId(), comment.getId(), reComment.getId()));
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.deleteReComment(post.getId() + 1L, comment.getId(), reComment.getId()));
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.deleteReComment(post.getId(), comment.getId() + 1L, reComment.getId()));
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.deleteReComment(post.getId(), comment.getId(), reComment.getId() + 1L));
+    }
 
     @Test
     @DisplayName("createComment")
@@ -61,6 +98,54 @@ class CommentServiceTest {
         assertEquals(actualComment, post.getComments().get(0));
     }
 
+    @Test
+    public void editComment() throws Exception {
+        // given
+        Post post = Post.builder().id(ID1).build();
+        Comment comment = Comment.builder().id(ID1).content(COMMENT_CONTENT).post(post).build();
+        String newContent = COMMENT_CONTENT + "2";
 
+        given(commentRepository.findById(comment.getId()))
+                .willReturn(Optional.of(comment));
+        given(commentRepository.findById(ID2))
+                .willReturn(Optional.empty());
+
+        // when
+        Comment returnedComment = commentService.editComment(post.getId(), comment.getId(), newContent);
+
+        // then
+        assertThat(returnedComment).isEqualTo(comment);
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.editComment(post.getId(), ID2, newContent));
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.editComment(ID2, comment.getId(), newContent));
+    }
+
+    @Test
+    public void editReComment() throws Exception {
+        // given
+        Post post = Post.builder().id(ID1).build();
+        Comment comment = Comment.builder().id(ID1).content(COMMENT_CONTENT).post(post).build();
+        ReComment reComment = ReComment.builder().id(ID1).content(COMMENT_CONTENT).comment(comment).build();
+        String newContent = COMMENT_CONTENT + "2";
+
+        given(reCommentRepository.findById(comment.getId()))
+                .willReturn(Optional.of(reComment));
+        given(reCommentRepository.findById(ID2))
+                .willReturn(Optional.empty());
+
+        // when
+        ReComment returnedReComment =
+                commentService.editReComment(post.getId(), comment.getId(), reComment.getId(), newContent);
+
+        // then
+        assertThat(returnedReComment).isEqualTo(reComment);
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.editReComment(post.getId(), comment.getId(), ID2, newContent));
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.editReComment(post.getId(), ID2, reComment.getId(), newContent));
+        assertThrows(BusinessLogicException.class,
+                () -> commentService.editReComment(ID2, comment.getId(), reComment.getId(), newContent));
+    }
 
 }

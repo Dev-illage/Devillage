957ebc0b9333449347581e67dc152eca3891c635
yangddoddi <pud1251@gmail.com>
2022-09-29 23:40:09 +0900

    Refactor : JwtConstants에 필요 없는 상수 제거 및 Redirect URL 추가
    전혀 사용되지 않고 있는 상수들을 모두 제거하고 OAuth에 필요한 URL을 추가했습니다.

--- src/main/java/com/devillage/teamproject/security/util/JwtConstants.java
+++ src/main/java/com/devillage/teamproject/security/util/JwtConstants.java
@@ -3,15 +3,12 @@ package com.devillage.teamproject.security.util;
 public class JwtConstants {
     public final static String SECRET_KEY = "antmsEmtdlswlrndrmagkrpTsyd";
     public final static String REFRESH_SECRET_KEY = "dlrjsejejdnrrndrmagkrpTshd";
-    public final static Long ACCESS_TOKEN_EXPIRE_COUNT = 60000L*10; // 10 minutes
+    public final static Long ACCESS_TOKEN_EXPIRE_COUNT = 60000L*10*3; // 30 minutes
     public final static Long REFRESH_TOKEN_EXPIRE_COUNT = 7 * 24 * 60 * 60 * 1000L; // 7 days
     public final static String BEARER_TYPE = "Bearer ";
-    public final static String AUTHORITIES_KEY = "auth";
     public final static String AUTHORIZATION_HEADER = "Authorization";
     public final static String REFRESH_HEADER = "RefreshToken";
-    public final static String TOKEN_EXPIRED = "tokenExpired";
-    public final static String TOKEN_OK = "tokenOk";
     public final static String ROLES = "roles";
     public final static String SEQUENCE = "sequence";
-    public final static String REDIRECT_URL_OAUTH2 = "http://localhost:3000/";
+    public final static String REDIRECT_URL_OAUTH2 = "http://localhost:3000/login/oauth";
 }

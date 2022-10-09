b8ff1cb7ba2e323aca17ec2228717f418fbb6178
kkg5 <kkg05102@gmail.com>
2022-10-09 15:53:14 +0900

    Refactor: PostControllerImpl > getUser().getId() -> userInfo.getId()

--- src/main/java/com/devillage/teamproject/controller/post/PostControllerImpl.java
+++ src/main/java/com/devillage/teamproject/controller/post/PostControllerImpl.java
@@ -42,7 +42,7 @@ public class PostControllerImpl implements PostController {
 
         Bookmark bookmark = postService.postBookmark(userInfo.getId(), postId);
         return PostDto.Response.BookmarkDto.of(
-                bookmark.getUser().getId(),
+                userInfo.getId(),
                 bookmark.getPost().getId(),
                 bookmark.getId());
     }
@@ -52,7 +52,7 @@ public class PostControllerImpl implements PostController {
 
         ReportedPost reportedPost = postService.postReport(userInfo.getId(), postId);
         return PostDto.Response.ReportDto.of(
-                reportedPost.getUser().getId(),
+                userInfo.getId(),
                 reportedPost.getPost().getId(),
                 reportedPost.getId());
     }
@@ -62,7 +62,7 @@ public class PostControllerImpl implements PostController {
 
         Post post = postService.postLike(userInfo.getId(), postId);
         return PostDto.Response.LikeDto.of(
-                post.getUser().getId(),
+                userInfo.getId(),
                 post.getId(),
                 post.getLikeCount());
     }

fd980e777b2f396373ab74bea3c1057778000189
Qone2 <whrbdnjs33@naver.com>
2022-10-09 16:30:45 +0900

    Feat : 이전 커밋의 추가사항

---
+++ src/main/java/com/devillage/teamproject/controller/file/LocalImageController.java
@@ -0,0 +1,21 @@
+package com.devillage.teamproject.controller.file;
+
+import com.devillage.teamproject.dto.FileDto;
+
+public class LocalImageController implements FileController {
+
+    @Override
+    public Long postFile() {
+        return null;
+    }
+
+    @Override
+    public FileDto.Response getFile(String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteFile() {
+
+    }
+}

3c0776419122b1fd8625acdfb65f0611535d103a
kkg5 <kkg05102@gmail.com>
2022-10-13 00:20:36 +0900

    Feat: ChatService 작성

---
+++ src/main/java/com/devillage/teamproject/service/chat/ChatService.java
@@ -0,0 +1,10 @@
+package com.devillage.teamproject.service.chat;
+
+import com.devillage.teamproject.entity.ChatRoom;
+
+import java.util.List;
+
+public interface ChatService {
+    List<ChatRoom> getRooms();
+    ChatRoom getRoom(Long userId, String roomName);
+}
---
+++ src/main/java/com/devillage/teamproject/service/chat/ChatServiceImpl.java
@@ -0,0 +1,43 @@
+package com.devillage.teamproject.service.chat;
+
+import com.devillage.teamproject.entity.ChatIn;
+import com.devillage.teamproject.entity.ChatRoom;
+import com.devillage.teamproject.repository.chat.ChatRoomRepository;
+import com.devillage.teamproject.entity.User;
+import com.devillage.teamproject.exception.BusinessLogicException;
+import com.devillage.teamproject.exception.ExceptionCode;
+import com.devillage.teamproject.service.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ChatServiceImpl implements ChatService {
+    private final UserService userService;
+    private final ChatRoomRepository chatRoomRepository;
+
+    @Override
+    public List<ChatRoom> getRooms() {
+        return chatRoomRepository.findAll();
+    }
+
+    @Override
+    public ChatRoom getRoom(Long userId, String roomName) {
+        User user = userService.findVerifiedUser(userId);
+        ChatRoom chatRoom = chatRoomRepository.findByRoomName(roomName)
+                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.CHAT_ROOM_NOT_FOUND));
+
+        List<User> users = chatRoom.getChatIns().stream()
+                .map(ChatIn::getUser)
+                .collect(Collectors.toList());
+
+        if (!users.contains(user)) {
+            throw new BusinessLogicException(ExceptionCode.UNAUTHORIZED_FOR_CHATROOM_EXCEPTION);
+        }
+
+        return chatRoom;
+    }
+}

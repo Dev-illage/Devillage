a48869e9fec73ca9d20319bc3fe252709d6f547c
kkg5 <kkg05102@gmail.com>
2022-10-13 11:15:11 +0900

    Feat: ChatRoomRepository > existsByRoomName 추가, ChatInRepository 작성

---
+++ src/main/java/com/devillage/teamproject/repository/chat/ChatInRepository.java
@@ -0,0 +1,8 @@
+package com.devillage.teamproject.repository.chat;
+
+
+import com.devillage.teamproject.entity.ChatIn;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatInRepository extends JpaRepository<ChatIn, Long> {
+}
--- src/main/java/com/devillage/teamproject/repository/chat/ChatRoomRepository.java
+++ src/main/java/com/devillage/teamproject/repository/chat/ChatRoomRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByRoomName(String roomName);
+    Boolean existsByRoomName(String roomName);
 }

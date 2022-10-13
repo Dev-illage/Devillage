48ddd4ae509208129c6a8e1059fe34a6ba1ef681
Qone2 <whrbdnjs33@naver.com>
2022-10-13 22:45:16 +0900

    Fix : 게시글 조회시 댓글의 2페이지부터 보이던 문제 수정

--- src/main/java/com/devillage/teamproject/controller/post/PostControllerImpl.java
+++ src/main/java/com/devillage/teamproject/controller/post/PostControllerImpl.java
@@ -30,7 +30,7 @@ public class PostControllerImpl implements PostController {
     @Override
     public SingleResponseDto<PostDto.Response.PostDetail> getPost(AuthDto.UserInfo userInfo, Long postId) {
         Post post = postService.getPost(postId);
-        Page<Comment> commentPage = commentService.findComments(postId, 1, 10);
+        Page<Comment> commentPage = commentService.findComments(postId, 0, 10);
         return SingleResponseDto.of(PostDto.Response.PostDetail.of(post, commentPage,
                 userInfo == null ? (Long) 0L : userInfo.getId()));
     }

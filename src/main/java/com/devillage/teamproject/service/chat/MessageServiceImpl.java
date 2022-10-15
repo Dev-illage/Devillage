52de39676de354f53c846d0b9c26ebaafe9850c3
kkg5 <kkg05102@gmail.com>
2022-10-15 12:41:42 +0900

    Fix: MessageService > joinUserChatRoom 수정

--- src/main/java/com/devillage/teamproject/service/chat/MessageServiceImpl.java
+++ src/main/java/com/devillage/teamproject/service/chat/MessageServiceImpl.java
@@ -60,7 +60,9 @@ public class MessageServiceImpl implements MessageService {
 
     private void joinUserChatRoom(User user, ChatRoom chatRoom) {
         chatInRepository.findFirstByUserAndChatroom(user, chatRoom)
-                .orElseThrow(() -> new BusinessLogicException(ExceptionCode.CHAT_IN_ALREADY_EXISTS));
+                .ifPresent((chatIn) -> {
+                    throw new BusinessLogicException(ExceptionCode.CHAT_IN_ALREADY_EXISTS);
+                });
         chatInRepository.save(new ChatIn(user, chatRoom));
     }
 

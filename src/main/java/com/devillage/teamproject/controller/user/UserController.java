5920f9b2bcf975e790b93b03f80ed935774ea986
Qone2 <whrbdnjs33@naver.com>
2022-10-16 23:49:56 +0900

    Feat : user avatar 컨트롤러 디자인

--- src/main/java/com/devillage/teamproject/controller/user/UserController.java
+++ src/main/java/com/devillage/teamproject/controller/user/UserController.java
@@ -1,12 +1,14 @@
 package com.devillage.teamproject.controller.user;
 
 import com.devillage.teamproject.dto.AuthDto;
+import com.devillage.teamproject.dto.FileDto;
 import com.devillage.teamproject.dto.SingleResponseDto;
 import com.devillage.teamproject.dto.UserDto;
 import com.devillage.teamproject.security.resolver.AccessToken;
 import com.devillage.teamproject.security.util.JwtConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -37,4 +39,13 @@ public interface UserController {
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     void deleteUser(@RequestHeader(JwtConstants.AUTHORIZATION_HEADER) String token);
+
+    @PostMapping("/profile/{user-id}/avatar")
+    @ResponseStatus(HttpStatus.CREATED)
+    FileDto.SimpleResponse postAvatar(@AccessToken AuthDto.UserInfo userInfo, @PathVariable("user-id") Long userId,
+                                      @RequestPart MultipartFile imageFile);
+
+    @DeleteMapping("/profile/{user-id}/avatar")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deleteUser(@AccessToken AuthDto.UserInfo userInfo, @PathVariable("user-id") Long userId);
 }

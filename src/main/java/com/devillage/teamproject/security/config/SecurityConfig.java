449f2cb37d6f7f8f48351f69fb34714ca4e5a50f
Qone2 <whrbdnjs33@naver.com>
2022-10-18 01:53:48 +0900

    Feat : 유저프로필에 사진추가 시큐리티설정 변경

--- src/main/java/com/devillage/teamproject/security/config/SecurityConfig.java
+++ src/main/java/com/devillage/teamproject/security/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig {
                 .mvcMatchers(GET, "/users/**").hasAnyRole("USER","MANAGER","ADMIN")
                 .mvcMatchers(DELETE,"/users/**").hasAnyRole("USER","MANAGER","ADMIN")
                 .mvcMatchers(PATCH,"/users/**").hasAnyRole("USER","MANAGER","ADMIN")
+                .mvcMatchers(POST,"/users/profile/**").hasAnyRole("USER","MANAGER","ADMIN")
                 .mvcMatchers(GET,"/files/**").permitAll()
                 .mvcMatchers(POST,"/files/**").hasAnyRole("USER","MANAGER","ADMIN")
                 .mvcMatchers(GET, "/test/**").hasAnyRole("USER","MANAGER","ADMIN")

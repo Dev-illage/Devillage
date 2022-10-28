59ea471f5e43e46ce30f23425be2e141bd67f928
yangddoddi <pud1251@gmail.com>
2022-10-28 13:35:10 +0900

    Fix : 패턴 문제 수정 및 상수 추가

---
+++ src/main/java/com/devillage/teamproject/dto/ValidConstants.java
@@ -0,0 +1,8 @@
+package com.devillage.teamproject.dto;
+
+public class ValidConstants {
+    public static final String INVALID_EMAIL = "INVALID_EMAIL";
+    public static final String INVALID_PASSWORD = "INVALID_PASSWORD";
+    public static final String INVALID_NICKNAME = "INVALID_NICKNAME";
+    public static final String CAN_NOT_NULL = "CAN_NOT_NULL_OR_EMPTY";
+}
